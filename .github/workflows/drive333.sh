0379c26b2d2675d58600a4a82ee021c598c45d51
howtogo2303 <126365147+howtogo2303@users.noreply.github.com>
2024-02-12 11:15:21 +0800

    Update drive333.sh

--- .github/workflows/drive333.sh
+++ .github/workflows/drive333.sh
@@ -2,10 +2,10 @@
 wget https://raw.githubusercontent.com/MoeClub/OneList/master/OneDriveUploader/amd64/linux/OneDriveUploader -P /usr/local/bin/
 chmod +x /usr/local/bin/OneDriveUploader
 OneDriveUploader -c /home/runner/work/0127/0127/bftyuip611.json -s "/home/runner/work/0127"
-OneDriveUploader -c /home/runner/work/0127/0127/cccgoogle.json -s "home/runner/work/0127"
-OneDriveUploader -c /home/runner/work/0127/0127/gyui22255.json -s "home/runner/work/0127"
-OneDriveUploader -c /home/runner/work/0127/0127/dtyiop709.json -s "home/runner/work/0127"
-OneDriveUploader -c /home/runner/work/0127/0127/tyvyuiop709.json -s "home/runner/work/0127"
-OneDriveUploader -c /home/runner/work/0127/0127/gothyy610.json -s "home/runner/work/0127"
-OneDriveUploader -c /home/runner/work/0127/0127/northtty612.json -s "home/runner/work/0127"
-OneDriveUploader -c /home/runner/work/0127/0127/amertty612.json -s "home/runner/work/0127"
+OneDriveUploader -c /home/runner/work/0127/0127/cccgoogle.json -s "/home/runner/work/0127"
+OneDriveUploader -c /home/runner/work/0127/0127/gyui22255.json -s "/home/runner/work/0127"
+OneDriveUploader -c /home/runner/work/0127/0127/dtyiop709.json -s "/home/runner/work/0127"
+OneDriveUploader -c /home/runner/work/0127/0127/tyvyuiop709.json -s "/home/runner/work/0127"
+OneDriveUploader -c /home/runner/work/0127/0127/gothyy610.json -s "/home/runner/work/0127"
+OneDriveUploader -c /home/runner/work/0127/0127/northtty612.json -s "/home/runner/work/0127"
+OneDriveUploader -c /home/runner/work/0127/0127/amertty612.json -s "/home/runner/work/0127"
