c980882d8adbf43ca9c51d1c619f0d8754936abd
howtogo2303 <126365147+howtogo2303@users.noreply.github.com>
2024-02-12 10:11:29 +0800

    Update drive333.sh

--- .github/workflows/drive333.sh
+++ .github/workflows/drive333.sh
@@ -3,9 +3,13 @@ wget https://raw.githubusercontent.com/MoeClub/OneList/master/OneDriveUploader/a
 chmod +x /usr/local/bin/OneDriveUploader
 pwd
 OneDriveUploader -c /home/runner/work/0127/0127/bftyuip611.json -s "/home/runner/work/0127"
+pwd
 OneDriveUploader -c /home/runner/work/0127/0127/cccgoogle.json -s "home/runner/work/0127"
+pwd
 OneDriveUploader -c /home/runner/work/0127/0127/gyui22255.json -s "home/runner/work/0127"
+
 OneDriveUploader -c /home/runner/work/0127/0127/dtyiop709.json -s "home/runner/work/0127"
+pwd
 OneDriveUploader -c /home/runner/work/0127/0127/tyvyuiop709.json -s "home/runner/work/0127"
 OneDriveUploader -c /home/runner/work/0127/0127/gothyy610.json -s "home/runner/work/0127"
 OneDriveUploader -c /home/runner/work/0127/0127/northtty612.json -s "home/runner/work/0127"
