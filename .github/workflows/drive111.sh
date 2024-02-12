9a1f2f0af8a5611ff15c4de6651907b27e49f327
howtogo2303 <126365147+howtogo2303@users.noreply.github.com>
2024-02-12 09:37:14 +0800

    Update drive111.sh

--- .github/workflows/drive111.sh
+++ .github/workflows/drive111.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+wget https://raw.githubusercontent.com/MoeClub/OneList/master/OneDriveUploader/amd64/linux/OneDriveUploader -P /usr/local/bin/
+chmod +x /usr/local/bin/OneDriveUploader
+pwd
 OneDriveUploader -c /home/runner/work/0127/0127/bftyuip611.json -s "/home/runner/work/0127"
 OneDriveUploader -c gyui22255.json -s "goor"
 OneDriveUploader -c dtyiop709.json -s "goor"
