003388493bde91e1346e63b3a29834dfea28cff8
howtogo2303 <126365147+howtogo2303@users.noreply.github.com>
2024-02-12 09:51:20 +0800

    Update drive333.sh

--- .github/workflows/drive333.sh
+++ .github/workflows/drive333.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+wget https://raw.githubusercontent.com/MoeClub/OneList/master/OneDriveUploader/amd64/linux/OneDriveUploader -P /usr/local/bin/
+chmod +x /usr/local/bin/OneDriveUploader
+pwd
