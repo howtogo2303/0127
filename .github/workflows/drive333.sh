99fba1a9d170bd1778da174bbf050dd714e71f1a
howtogo2303 <126365147+howtogo2303@users.noreply.github.com>
2024-02-12 11:36:55 +0800

    Update drive333.sh

--- .github/workflows/drive333.sh
+++ .github/workflows/drive333.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-OneDriveUploader -c /home/runner/work/0127/0127/www/230117/bftyuip611.json -s "/home/runner/work/0127"
-OneDriveUploader -c /home/runner/work/0127/0127/www/230117/dtyiop709.json -s "/home/runner/work/0127"
+OneDriveUploader -c /home/runner/work/0127/0127/www/230117/bftyuip611.json -s "/home/runner/work/0127/0127/www"
+OneDriveUploader -c /home/runner/work/0127/0127/www/230117/dtyiop709.json -s "/home/runner/work/0127/0127/www"
 OneDriveUploader -c /home/runner/work/0127/0127/gyui22255.json -s "/home/runner/work/0127"
 OneDriveUploader -c /home/runner/work/0127/0127/dtyiop709.json -s "/home/runner/work/0127"
 OneDriveUploader -c /home/runner/work/0127/0127/tyvyuiop709.json -s "/home/runner/work/0127"
